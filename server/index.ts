5afd800eb3e5143a81037fb1e59650b954366c32
Gon <gon.mendez13@gmail.com>
2022-07-04 16:01:09 -0300

    dist reference changes

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ import * as express from "express"
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static("dist"))
+app.use(express.static("../dist"))
 app.use(express.json());
 app.use(cors());
 
